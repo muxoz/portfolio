2f765c8c29592b970be30514b5dc4e7c81ea99e7
estivenm0 <estivenyeizon@gmail.com>
2025-08-21 19:05:13 -0500

    refactor: 💡 data projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,15 +9,6 @@ export const projects = [
     link: 'https://github.com/muxzz/offerty',
   },
   {
-    stack: ['Vue', 'TailwindCSS', 'Laravel', 'API'],
-    title: 'Aventuro',
-    img: '/projects/aventuro.webp',
-    description:
-      'Aventuro es una aplicación web para agencias de viajes que permite a los usuarios explorar varios paquetes de viajes, realizar reservas y administrar sus cuentas de manera eficiente. Diseñado pensando en la experiencia del usuario, Aventuro ofrece una gama de funcionalidades para garantizar un proceso de reserva fluido y agradable.',
-    repository: 'https://github.com/muxzz/aventuro',
-    link: 'https://youtu.be/enFP3gEd3sQ',
-  },
-  {
     stack: ['Laravel', 'Livewire', 'Alpine.js', 'TailwindCSS', 'MySQL'],
     title: 'Comprana',
     img: '/projects/comprana.webp',
@@ -27,6 +18,15 @@ export const projects = [
     link: 'https://youtu.be/ZDsoQOYZTCI',
   },
   {
+    stack: ['Vue', 'TailwindCSS', 'Laravel', 'API'],
+    title: 'Aventuro',
+    img: '/projects/aventuro.webp',
+    description:
+      'Aventuro es una aplicación web para agencias de viajes que permite a los usuarios explorar varios paquetes de viajes, realizar reservas y administrar sus cuentas de manera eficiente. Diseñado pensando en la experiencia del usuario, Aventuro ofrece una gama de funcionalidades para garantizar un proceso de reserva fluido y agradable.',
+    repository: 'https://github.com/muxzz/aventuro',
+    link: 'https://youtu.be/enFP3gEd3sQ',
+  },
+  {
     stack: ['Bootstrap', 'Alpine.js'],
     title: 'Avatarmaker',
     img: '/projects/avatarmaker.webp',
@@ -34,14 +34,5 @@ export const projects = [
       'Avatarmaker es una web que permite crear y descargar avatares personalizados mediante la API de DiceBear.',
     repository: 'https://github.com/muxzz/avatarmaker',
     link: 'https://avatarsmaker.netlify.app/',
-  },
-  {
-    stack: ['HTML', 'CSS', 'JavaScript'],
-    title: 'Pokedex',
-    img: '/projects/pokedex.webp',
-    description:
-      'Pokédex es una web con un diseño fiel al estilo original, que permite explorar información de Pokémon de forma visual y organizada.',
-    repository: 'https://github.com/muxzz/pokedex',
-    link: 'https://s-pokedex.netlify.app/',
   },
 ];
